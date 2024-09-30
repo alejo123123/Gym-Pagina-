2bafbf6971a680875ab1c91ced8656d64105b7cf
Alejo <alejobustamanter@gmail.com>
2024-09-30 14:23:39 -0500

    parte donde se elige que ejericcios desea visualizar

---
+++ ejercicios.php
@@ -0,0 +1,21 @@
+<?php 
+  include './includes/templates/header.php';
+?>
+
+<body>
+<div class="split-screen">
+  <div class="panel panel-superior">
+    <div class="panel-content">
+      <h2 class="panel-title">Superior</h2>
+      <button class="panel-btn" onclick="window.location.href='includes/templates/superior.php'">Entrenar</button>
+    </div>
+  </div>
+  <div class="panel panel-inferior">
+    <div class="panel-content">
+      <h2 class="panel-title">Inferior</h2>
+      <button class="panel-btn" onclick="window.location.href='includes/templates/inferior.php'">Entrenar</button>
+    </div>
+  </div>
+</div>
+</body>
+</html>
